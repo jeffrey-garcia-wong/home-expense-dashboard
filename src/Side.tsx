70d2c711d1850d874fdecbfd21b7582fa5eae74f
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-09-05 10:44:27 +0100

    add chartjs for react and initial charting for expenses

---
+++ src/Side.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import './Side.css';
+
+class Side extends Component {
+  render() {
+    return (
+      <div className="aside">
+        <h2>Expenses</h2>
+        <p>Household Expenditures</p>
+        <h2>Investment</h2>
+        <p>Investment on Equity or Funds</p>
+        <h2>Outstanding</h2>
+        <p>Outstanding Payments</p>
+      </div>
+    )
+  }
+}
+
+export default Side
