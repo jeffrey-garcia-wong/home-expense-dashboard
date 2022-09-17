62dbdbb688f520670c6b5d046f7413981bdb3a88
jeffrey garcia <jeffrey-garcia@outlook.com>
2022-09-17 10:04:47 +0100

    add data aggregation logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,114 +3,27 @@ import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Main from "./Main";
+
 import MonthlyOverallExp from "./pages/expenses/MonthlyOverallExp";
+import MonthlyFixedExp from "./pages/expenses/MonthlyFixedExp";
 import MonthlyUtilityExp from "./pages/expenses/MonthlyUtilityExp";
 import MonthlyMiscExp from "./pages/expenses/MonthlyMiscExp";
 
+import ExpenseData from "./Data";
+
 import './App.css';
 
-class App1 extends Component<any> {
+class App extends Component<any> {
   render() {
+    // const init = (() => {
+    //   console.log(`${JSON.stringify(ExpenseData.fixedExpenses)}`);
+    // })();
+
     const data = {
-      overallExpenses: {
-        labels: ['Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-        datasets: [
-            {
-                label: 'Fixed expenses',
-                data: [2150.84, 2150.84, 2150.84, 2150.84, 2150.84, 2167.08],
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderWidth: 2
-            },
-            {
-                label: 'Utility expenses',
-                data: [470.96, 428.47, 560.82, 529.97, 538.77, 398.82],
-                borderColor: 'rgba(255, 206, 86)',
-                backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                borderWidth: 2
-            },
-            {
-                label: 'Miscellaneous expenses',
-                data: [1029.69, 1578.91, 1420.97, 3119.75, 5493.09, 3841.80],
-                borderColor: 'rgba(75, 192, 192)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderWidth: 2
-            },              
-        ]
-      },
-      utilityExpenses: {
-        labels: ['Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-        datasets: [
-          {
-              label: 'Electricity',
-              data: [71.92, 71.92, 71.92, 71.92, 71.92, 71.92],
-              borderColor: 'rgb(54, 162, 235, 1)',
-              backgroundColor: 'rgba(54, 162, 235, 0.2)',
-              borderWidth: 2
-          },
-          {
-              label: 'Gas',
-              data: [41.03, 41.03, 41.03, 41.03, 41.03, 41.03],
-              borderColor: 'rgba(255, 206, 86, 1)',
-              backgroundColor: 'rgba(255, 206, 86, 0.2)', 
-              borderWidth: 2               
-          },
-          {
-              label: 'Water',
-              data: [47.52, 47.52, 47.52, 47.52, 47.52, 47.52],
-              borderColor: 'rgba(75, 192, 192, 1)',
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderWidth: 2
-          },
-          {
-              label: 'Petrol',
-              data: [168.41, 125.92, 258.27, 227.42, 236.22, 96.27],
-              borderColor: 'rgba(153, 102, 255, 1)',
-              backgroundColor: 'rgba(153, 102, 255, 0.2)',
-              borderWidth: 2
-          },              
-        ]      
-      },
-      miscExpenses: {
-        labels: ['Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
-        datasets: [
-          {
-              label: 'Marketing',
-              data: [478.86, 772.34, 682.25, 561.78, 683.44, 559.43],
-              borderColor: 'rgb(191, 255, 0, 1)',
-              backgroundColor: 'rgba(191, 255, 0, 0.2)',
-              borderWidth: 2
-          },
-          {
-              label: 'Outdoor Dining',
-              data: [249.83, 109.31, 380.46, 463.99, 345.15, 181.49],
-              borderColor: 'rgba(255, 206, 86, 1)',
-              backgroundColor: 'rgba(255, 206, 86, 0.2)',
-              borderWidth: 2
-          },
-          {
-              label: 'Appliances',
-              data: [0.00, 150.00, 0.00, 2093.98, 1295.50, 745.50],                
-              borderColor: 'rgba(75, 192, 192, 1)',
-              backgroundColor: 'rgba(75, 192, 192, 0.2)',
-              borderWidth: 2
-          },            
-          {
-              label: 'Home Refurbishment',
-              data: [0.00, 55.00, 0.00, 0.00, 2825.00, 1862.78],
-              borderColor: 'rgba(255, 99, 132, 1)',
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderWidth: 2
-          },       
-          {
-              label: 'School & Activities',
-              data: [301.00, 542.26, 528.26, 170.00, 514.00, 612.60],
-              borderColor: 'rgba(153, 102, 255, 1)',
-              backgroundColor: 'rgba(153, 102, 255, 0.2)',
-              borderWidth: 2
-          },
-        ]
-      }           
+      overallExpenses: ExpenseData.overallExpenses,
+      fixedExpenses: ExpenseData.fixedExpenses,
+      utilityExpenses: ExpenseData.utilityExpenses,
+      miscExpenses: ExpenseData.miscExpenses
     };
 
     return (
@@ -119,6 +32,7 @@ class App1 extends Component<any> {
           <Route path="/" element={<Main />}>
             <Route index element={<MonthlyOverallExp expensesData={data.overallExpenses} />} />
             <Route path="monthlyOverallExp" element={<MonthlyOverallExp expensesData={data.overallExpenses} />} />
+            <Route path="monthlyFixedExp" element={<MonthlyFixedExp expensesData={data.fixedExpenses} />} />
             <Route path="monthlyUtilityExp" element={<MonthlyUtilityExp expensesData={data.utilityExpenses} />} />
             <Route path="monthlyMiscExp" element={<MonthlyMiscExp expensesData={data.miscExpenses} />} />
             {/* <Route path="*" element={<NoPage />} /> */}
@@ -129,20 +43,4 @@ class App1 extends Component<any> {
   }
 }
 
-const App = () => {
-    return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route index element={<MonthlyOverallExp />} />
-            <Route path="monthlyOverallExp" element={<MonthlyOverallExp />} />
-            <Route path="monthlyUtilityExp" element={<MonthlyUtilityExp />} />
-            <Route path="monthlyMiscExp" element={<MonthlyMiscExp />} />
-            {/* <Route path="*" element={<NoPage />} /> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-};
-
-export default App1;
+export default App;
---
+++ src/pages/expenses/MonthlyFixedExp.tsx
@@ -0,0 +1,186 @@
+import { MouseEvent } from "react";
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,  
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement,
+    BarElement,
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+
+import Side from '../../Side';
+
+import './MonthlyExp.css';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement,
+    BarElement,
+);
+
+const MonthlyFixedExp = ((props:any) => {
+    const expData = props.expensesData;
+
+    const barOptions = {
+        responsive: true,
+        scales: {
+            x: {
+                stacked: true,
+                grid: {
+                    drawBorder: true,
+                    drawOnChartArea: true,
+                    color: '#666666',                    
+                }                
+            },
+            y: {
+                stacked: true,
+                grid: {
+                    drawBorder: true,
+                    drawOnChartArea: true,
+                    color: '#666666',                    
+                },                
+                ticks: {
+                    stepSize: 100
+                }                    
+            },
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    footer: function(items:any) {
+                        var total = 0;
+                        for (var i = 0; i < expData['datasets'].length; i++) {
+                            total += expData['datasets'][i].data[items[0].dataIndex];
+                        }
+                        return 'Total: ' + total 
+                      }
+                }
+            },
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Monthly overall expenses in GBP (£)',
+            },
+        },     
+    };
+
+    const lineOptions = {
+        animations: {
+            x: {
+                duration: 2500,
+                from: -1
+            },
+            y: {
+                duration: 0,
+                from: 0
+            }
+        },        
+        interaction: {
+            mode: 'index' as const,
+            intersect: false,
+        },
+        stacked: false,    
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+                labels: {
+                    usePointStyle: true,
+                },                      
+            },
+            title: {
+                display: true,
+                text: 'Misc. expenses trend in GBP (£)',
+            },
+        },   
+        scales: {
+            x: {
+                grid: {
+                    drawBorder: true,
+                    drawOnChartArea: true,
+                    color: '#666666',                    
+                }
+            },            
+            y: {
+                type: 'linear' as const,
+                display: true,
+                position: 'left' as const,
+                grid: {
+                    drawBorder: true,
+                    drawOnChartArea: true,
+                    color: '#666666',
+                },
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 50
+                },
+            }   
+        },            
+    }    
+
+    const handleTabClick = (ev:MouseEvent) => {
+        const tabItem = (ev.target as Element) as HTMLElement|null;
+        if (tabItem == null) return;
+        // console.log(tabItem.id);
+        document.getElementById(tabItem.id + "-content")!.className = "tabcontent active";
+        document.getElementById(tabItem.id)!.className = "tablinks active";
+
+        const tabContentItems = document.getElementsByClassName("tabcontent active");
+        if (tabContentItems!=null && tabContentItems.length>0) {
+            for (var i = 0; i < tabContentItems.length; i++) {
+                if (tabContentItems[i].id.indexOf(tabItem.id) < 0) {
+                    tabContentItems[i].className = "tabcontent";
+                }
+            }
+        }
+
+        const tabLinkItems = document.getElementsByClassName("tablinks active");
+        if (tabLinkItems!=null && tabLinkItems.length>0) {
+            for (var i = 0; i < tabLinkItems.length; i++) {
+                if (tabLinkItems[i].id.indexOf(tabItem.id) < 0) {
+                    tabLinkItems[i].className = "tablinks";
+                }
+            }
+        }        
+    };
+    
+    return (
+        <div>
+            <div className="col-6 col-s-9">
+                <h1>Monthly Fixed Expenses</h1>
+
+                <div className="tab">
+                    <button id="breakdown" className="tablinks active" onClick={handleTabClick}>Breakdown</button>
+                    <button id="trend" className="tablinks" onClick={handleTabClick}>Trend</button>
+                </div>
+
+                <div id="breakdown-content" className="tabcontent active">
+                    <Bar options={barOptions} data={expData} height="300" />
+                </div>
+
+                <div id="trend-content" className="tabcontent">
+                    <Line options={lineOptions} data={expData} height="300" />
+                </div>                
+            </div>
+            <div className="col-3 col-s-12">
+                <Side data={expData} />
+            </div>
+        </div>          
+    );    
+});
+
+export default MonthlyFixedExp;
